808fe19a4643abad64e415bc328b423114e09e6a
StevenR123 <72549890+StevenR123@users.noreply.github.com>
2022-08-25 13:54:09 -0400

    Editor builds page based on JSON
    Added functions to build board/elements based on JSON file;
    Added Documentaion

--- src/react-web/Editor.js
+++ src/react-web/Editor.js
@@ -11,15 +11,7 @@ import { useQuery } from "@tanstack/react-query";
 export default function Editor() {
   const parentStyle = { width: 100, height: 100 };
 
-  // const query = useQuery(['helloWorld'], async () => {
-  //   const response = await fetch('http://localhost:3001/')
-  //   const data = await response.json()
-  //   return data
-  // })
-
-  // var textTest = (query?.data?.hello)
-  // //console.log(textTest)
-
+  //Gets a JSON file from backend server
   const query = useQuery(['example'], async () => {
     const response = await fetch('http://localhost:3001/')
     const data = await response.json()
@@ -27,57 +19,75 @@ export default function Editor() {
     return data
   })
 
+  //Displays to console whether or not the query has loaded
   console.log(query.status)
-  
+
+  //Send a placeholder output to the main App() until the query has loaded
   if (query.status === "loading") {
-    return <p>Still loading...</p>
+    return <p className="loadin-text">Still loading...</p>
+  }
+
+  //Builds a StyleEditorBlock from information stored in the queried JSON file
+  function buildShape (i) {
+    return (
+      <StyleEditorBlock key={i}  
+        width={(query?.data?.elements[i].width)}
+        height={(query?.data?.elements[i].height)}
+        left={(query?.data?.elements[i].left)}
+        top={(query?.data?.elements[i].top)}
+        unit={(query?.data?.elements[i].unit)}
+        parentStyle={parentStyle}
+      >        
+        <div className={(query?.data?.elements[i].className)}>{(query?.data?.elements[i].text)}</div>
+      </StyleEditorBlock>
+    )
+  }
+
+  //Builds a TextEditorBlock from information stored in the queried JSON file
+  function buildtextBlock (i) {
+    return (
+        <TextEditorBlock key={i}  
+          width={(query?.data?.elements[i].width)}
+          height={(query?.data?.elements[i].height)}
+          top={(query?.data?.elements[i].top)}
+          left={(query?.data?.elements[i].left)}
+          parentStyle={parentStyle}
+          unit={(query?.data?.elements[i].unit)}
+          initialText={(query?.data?.elements[i].initialText)}
+          initialFontColor={(query?.data?.elements[i].initialFontColor)}
+          initialFontSize={(query?.data?.elements[i].initialFontSize)}
+          initialFontName={(query?.data?.elements[i].initialFontName)}
+          initialFontStyle={(query?.data?.elements[i].initialFontStyle)}
+        />
+    )
+  }
+
+  //Builds EditableBoard, including elements like StyleEditorBlock and TextEditorBlock, from queried JSON file
+  function buildBoard (i) {
+    //Builds all elements, textBlocks & shapes in the queried Json and adds them to an array
+    const elements = []
+    for (let elementInx = 0; elementInx < (query?.data?.elements).length; elementInx++) {
+        if ((query?.data?.elements[elementInx].elementType) === "shape") {
+          elements.push(buildShape(elementInx))
+        } else {
+          elements.push(buildtextBlock(elementInx))
+        }
+        
+      }
+
+    return (
+      <EditableBoard     
+        unit={(query?.data?.boards[i].unit)}
+        width={(query?.data?.boards[i].width)}
+        height={(query?.data?.boards[i].height)}
+        backgroundColor={(query?.data?.boards[i].backgroundColor)}
+      >
+        {elements}
+      </EditableBoard>
+    )
   }
 
   return (
-    <EditableBoard
-      unit={(query?.data?.blocks[0].unit)}
-      width={(query?.data?.blocks[0].width)}
-      height={(query?.data?.blocks[0].height)}
-      backgroundColor={(query?.data?.blocks[0].backgroundColor)}
-
-      // unit={"rem"}
-      // width={parentStyle.width}
-      // height={parentStyle.height}
-      // backgroundColor={"#F3F0D1"}
-    >
-      <TextEditorBlock
-        width={16.5}
-        height={7}
-        top={3}
-        left={5.5}
-        parentStyle={parentStyle}
-        unit={"rem"}
-        initialText={"textTest"}
-        initialFontColor={"#ffffff"}
-        initialFontSize={0.5}
-        initialFontName={"stix-two-text"}
-        initialFontStyle={"twin-color-text"}
-      />
-      <StyleEditorBlock
-        width={7}
-        height={7}
-        left={7.2}
-        top={8}
-        unit={"rem"}
-        parentStyle={parentStyle}
-      >
-        <div className="pink circle">Drag</div>
-      </StyleEditorBlock>
-      <StyleEditorBlock
-        width={5.5}
-        height={5.5}
-        left={12.5}
-        top={11}
-        unit={"rem"}
-        parentStyle={parentStyle}
-      >
-        <div className="blue circle">Me</div>
-      </StyleEditorBlock>
-    </EditableBoard>
-  );
+    buildBoard(0)
+    );
 }  
