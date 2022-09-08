303a71008a19c4c53f6c40253dfc5d8b3b338f86
StevenR123 <72549890+StevenR123@users.noreply.github.com>
2022-09-08 12:16:02 -0400

    Revert "My best attempt at making sending JSON to localhost3001 work, but 3001 is not even recognized by React."
    This reverts commit c8ede48e24009666174e6e5a1a2b3a5bfb722236.

--- src/react-web/Editor.js
+++ src/react-web/Editor.js
@@ -1,6 +1,4 @@
 //import { isValidDateValue } from "@testing-library/user-event/dist/utils";
-import Cookies from 'js-cookie';
-
 import "./styles.css";
 
 import {buildBoard} from "./compBuilder"
@@ -58,31 +56,6 @@ function pullPage () {
     }
   }
 }
-
-let signal = {"boards":[{"boardId":1,"unit":"rem","left":0,"top":0,"width":50,"height":50,"backgroundColor":"#531fc2"},{"boardId":2,"unit":"rem","left":0,"top":50,"width":50,"height":50,"backgroundColor":"Red"}],"elements":[{"elementId":1,"elementType":"shape","width":7,"height":7,"left":7.2,"top":8,"unit":"rem","className":"pink circle","text":"Drag"},{"elementId":2,"elementType":"shape","width":5.5,"height":5.5,"left":12.5,"top":11,"unit":"rem","className":"blue circle","text":"me"},{"elementId":3,"elementType":"textBlock","width":23.125,"height":4.125,"top":19,"left":10.7875,"unit":"rem","initialText":"Hello Fellows","initialFontColor":"#96ffdc","initialFontSize":0.59,"initialFontName":"andada-pro","initialFontStyle":"twin-color-text"}]}
-
-const postRequest = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'X-CSRFToken': Cookies.get('csrftoken'),
-  },
-  body: JSON.stringify(signal),
-}
-
-const sendSignal = async () => {
-  const response = await fetch(`http://localhost:3001/`, postRequest);
-
-  if (!response.ok) {
-    throw new Error('Network response for primary data not ok!')
-  } else {
-    const responseData = await response.json();
-    console.log(responseData);
-    // setWordChoices([...wordChoices])
-  };
-}
-
-sendSignal();
 
 setTimeout(function() { pullPage(); }, 5000);
 setInterval(function(){
