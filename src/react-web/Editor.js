5cf8f60f846a8042cddaba785b6cb35e3077af73
SmashingPrograms <smashingprograms@gmail.com>
2022-09-14 16:54:29 -0400

    Add textbox / circle (but simultaneous somewhat)

--- src/react-web/Editor.js
+++ src/react-web/Editor.js
@@ -28,18 +28,28 @@ export default function Editor() {
   }
 
   //Used when adding shape
-  const handleSubmit = (e) => {
+  const circleHandleSubmit = (e) => {
     e.preventDefault();
 
     setEditableItems([...editableItems, {"elementId":1,"elementType":"shape","width":7,"height":7,"left":7.2,"top":8,"unit":"rem","className":"pink circle","text":name}]);
   }
 
     //Used to set text when adding shape
-  const handleInput = (e) => {
+  const circleHandleInput = (e) => {
+    setName(e.target.value);
+  }
+
+  const textHandleSubmit = (e) => {
+    e.preventDefault();
+
+    setEditableItems([...editableItems, {"elementId":3,"elementType":"textBlock","width":23.125,"height":4.125,"top":19,"left":10.7875,"unit":"rem","initialText":"Hello Fellows","initialFontColor":"#96ffdc","initialFontSize":0.59,"initialFontName":"andada-pro","initialFontStyle":"twin-color-text"}]);
+  }
+
+  const textHandleInput = (e) => {
     setName(e.target.value);
   }
 
   return (
-    buildPage(query, name, editableItems, handleSubmit, handleInput)
+    buildPage(query, name, editableItems, circleHandleSubmit, circleHandleInput, textHandleSubmit, textHandleInput)
     );
   }  
