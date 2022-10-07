57feb1b1306791387e0da2989ff24f6f5a0fd9d6
SmashingPrograms <smashingprograms@gmail.com>
2022-10-07 14:34:02 -0500

    React Router implemented, app is not breaking but the feature doesn't seem to work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './react-web/Comp/test';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import {
     QueryClient,
@@ -16,10 +18,23 @@ const queryClient = new QueryClient()
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <QueryClientProvider client={queryClient}>
-        <App />
-    </QueryClientProvider>,
-    rootElement);
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />}>
+                <Route path='test' element={<Test />} />
+                <Route path="*" element={
+                    <main style={{padding: "1rem"}}>
+                    <p>There's nothing here!</p>
+                    </main>
+                } />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+  </QueryClientProvider>,
+  rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/react-web/Comp/test.js
@@ -0,0 +1,12 @@
+function Test() {
+  return (
+    <div>
+      {
+      console.log('I got here')
+      }
+    I am the testity test
+    </div>
+  )
+}
+
+export default Test;
