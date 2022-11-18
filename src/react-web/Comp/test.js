be79f0a466306b97e1ff54d15bfe595a6b62797b
StevenR123 <72549890+StevenR123@users.noreply.github.com>
2022-11-18 20:29:12 +0000

    Changed Test.js Greeting

--- src/react-web/Comp/test.js
+++ src/react-web/Comp/test.js
@@ -1,10 +1,17 @@
 function Test() {
+  let greeting = "You aren't logged in."
+  if (localStorage.getItem('displayName') !== null){
+    greeting = "Hi " + localStorage.getItem('displayName')
+  } else if (localStorage.getItem('firstName') !== null && localStorage.getItem('lastName')!== null ) {
+    greeting = "Hi " + localStorage.getItem('firstName') + " " + localStorage.getItem('lastName')
+  }
+  
   return (
     <div className="testAbility">
       {
       console.log('I got here')
       }
-    Hi {localStorage.getItem('firstName') + " " + localStorage.getItem('lastName')}
+    {greeting}
     </div>
   )
 }
